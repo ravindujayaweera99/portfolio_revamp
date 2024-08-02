7c949f9e111202b409de8427c52e7bafb5adf7db
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-08-02 21:22:05 +0530

    Tailwind issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <h1 className="text-black">Hello World!</h1>
-    </>
+    <div className="font-primary">
+      <h1 className="text-red-500">Hello World!</h1>
+    </div>
   );
 }
 
